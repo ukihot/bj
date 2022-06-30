c181c6448e5b3d2e624b278d6f890ae40ae3447f
ukihot <tokunaga.y@tyco.co.jp>
2022-06-30 14:34:38 +0900

    wip

--- src/recipes/translater.rs
+++ src/recipes/translater.rs
@@ -5,15 +5,23 @@ pub fn soot_translate(hands: &Vec<u32>) -> Vec<String> {
             // スペード
             0..=12 => {
                 let n: &str = &(alias + 1).to_string();
-
-                with_soot.push("スペードの".to_string() + n);
+                with_soot.push("スペード♠の".to_string() + n);
             }
             // ハート
-            13..=25 => with_soot.push("ハート".to_string()),
+            13..=25 => {
+                let n: &str = &(alias - 12).to_string();
+                with_soot.push("ハート♡の".to_string() + n);
+            }
             // ダイヤ
-            26..=38 => with_soot.push("ダイヤ".to_string()),
+            26..=38 => {
+                let n: &str = &(alias - 25).to_string();
+                with_soot.push("ダイヤ♦の".to_string() + n);
+            }
             // クローバー
-            39..=51 => with_soot.push("クローバー".to_string()),
+            39..=51 => {
+                let n: &str = &(alias - 38).to_string();
+                with_soot.push("クローバー♧の".to_string() + n);
+            }
             // 想定外
             _ => panic!(),
         }
