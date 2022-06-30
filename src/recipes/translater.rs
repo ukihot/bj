0892a9618b58d67300dc5fd8fa683e663c49432b
ukihot <tokunaga.y@tyco.co.jp>
2022-06-30 14:29:19 +0900

    wip

--- src/recipes/translater.rs
+++ src/recipes/translater.rs
@@ -1,15 +1,19 @@
-pub fn soot_translate(hands: &Vec<u32>) -> Vec<&str> {
-    let mut with_soot: Vec<&str> = vec![];
+pub fn soot_translate(hands: &Vec<u32>) -> Vec<String> {
+    let mut with_soot: Vec<String> = vec![];
     for alias in hands.iter() {
         match alias {
             // スペード
-            0..=12 => with_soot.push("スペード"),
+            0..=12 => {
+                let n: &str = &(alias + 1).to_string();
+
+                with_soot.push("スペードの".to_string() + n);
+            }
             // ハート
-            13..=25 => with_soot.push("ハート"),
+            13..=25 => with_soot.push("ハート".to_string()),
             // ダイヤ
-            26..=38 => with_soot.push("ダイヤ"),
+            26..=38 => with_soot.push("ダイヤ".to_string()),
             // クローバー
-            39..=51 => with_soot.push("クローバー"),
+            39..=51 => with_soot.push("クローバー".to_string()),
             // 想定外
             _ => panic!(),
         }
