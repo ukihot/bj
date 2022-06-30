6fc57e819badfb1773c75e1fbdeb48784efad092
ukihot <tokunaga.y@tyco.co.jp>
2022-06-30 16:41:15 +0900

    だいたい完成

--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,37 @@ fn main() {
     preparation(&mut hands_player, 2);
     // ディーラーが1枚引いて準備
     preparation(&mut hands_dealer, 2);
-    // プレイヤーの1枚目と2枚目を表示
+    // プレイヤーの手札をすべて表示
     println!("あなたの手札：{:?}", soot_translate(&hands_player));
     // ディーラーの手札を1枚だけ表示
     println!("相手の手札：[{}]", soot_translate(&hands_dealer)[0]);
 
-    // ヒットかスタンドか選択
-    let mut hit_or_stand = String::new();
-
-    // 入力文字のバリデーション
-    while hit_ot_stand_validate(&hit_or_stand) {
-        let mut input_text = String::new();
-        std::io::stdin().read_line(&mut input_text).ok();
-        hit_or_stand = input_text.trim().to_string();
+    // ヒットを尋ね続ける
+    while ask_hit() == "h" {
+        println!("新しくカードを引きました。");
+        preparation(&mut hands_player, 1);
+        // プレイヤーの1枚目と2枚目を表示
+        println!("あなたの手札：{:?}", soot_translate(&hands_player));
     }
-    println!("{}です", hit_or_stand);
 
     // ヒットを選択して合計値が22以上ならバースト(プレイヤーの敗北)
     // ヒットを選択して合計値が20以下なら再度ヒット or スタンドを選択（ヒットは何回でもOK）
     // プレイヤーが合計値21以下で勝負を待っている状態になったらディーラーは合計値が17以上になるまで無条件にカードを引く
     // バーストしたらプレイヤーの勝利
     // プレイヤーとディーラーが引き終えたら勝負。より21に近い方の勝ち
+}
+
+fn ask_hit() -> String {
+    // ヒットかスタンドか選択
+    let mut hit_or_stand = String::new();
+
+    // 入力文字のバリデーション
+    while hit_or_stand_validate(&hit_or_stand) {
+        let mut input_text = String::new();
+        std::io::stdin().read_line(&mut input_text).ok();
+        hit_or_stand = input_text.trim().to_string();
+    }
+    hit_or_stand
 }
 
 /*
@@ -47,7 +57,7 @@ fn preparation(hands: &mut Vec<u32>, pull_max: u32) {
     });
 }
 
-fn hit_ot_stand_validate(c: &str) -> bool {
+fn hit_or_stand_validate(c: &str) -> bool {
     if c == "h" {
         false
     } else if c == "s" {
